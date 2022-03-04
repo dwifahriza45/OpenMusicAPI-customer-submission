0c791c6ddc88558c7c3a535e448629dea2226ca7
dwifahriza <max@MacBook-Air-Max.local>
2022-03-04 10:44:46 +0700

    fix customer

--- src/Listener.js
+++ src/Listener.js
@@ -10,12 +10,13 @@ class Listener {
     try {
       const { playlistId, targetEmail } = JSON.parse(message.content.toString());
 
-      // playlist.songs = await this._service.getSongsPlaylistHandler(id);
-
       const playlist = await this._songsService.getPlaylistById(playlistId);
       playlist.songs = await this._songsService.getPlaylist(playlistId);
+      const detail = {
+          playlist,
+      };
 
-      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlist));
+      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(detail));
       console.log(result);
     } catch (error) {
       console.log(error);
